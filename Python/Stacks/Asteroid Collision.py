4dd2cefe830e15481546d1bf1cac78fcc84c2f96
Mayur Vijay Bhat <mayappbhat@gmail.com>
2025-02-16 10:18:32 -0500

    Stack problem

---
+++ Python/Stacks/Asteroid Collision.py
@@ -0,0 +1,21 @@
+#Link: https://leetcode.com/problems/asteroid-collision/
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            poppedValue = 0
+            while stack and abs(asteroid) >= abs(stack[-1]) and (asteroid < 0 and stack[-1] > 0):
+                if abs(asteroid) > abs(stack[-1]):
+                    poppedValue = stack.pop()
+                elif abs(asteroid) == abs(stack[-1]):
+                    poppedValue = stack.pop()
+                    break
+            if not stack or (stack and ((asteroid < 0 and stack[-1] < 0) or (asteroid > 0 and stack[-1] > 0) or(asteroid > 0 and stack[-1] < 0))):
+                if poppedValue:
+                    if abs(asteroid) != abs(poppedValue):
+                        stack.append(asteroid)
+                else:
+                    stack.append(asteroid)
+            
+        return stack
