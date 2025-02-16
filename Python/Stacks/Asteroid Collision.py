0b04bf901d4df501380efeb9fbc255ce767071c0
Mayur Vijay Bhat <mayappbhat@gmail.com>
2025-02-16 10:26:20 -0500

    Changes

--- Python/Stacks/Asteroid Collision.py
+++ Python/Stacks/Asteroid Collision.py
@@ -19,3 +19,22 @@ class Solution:
                     stack.append(asteroid)
             
         return stack
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            while stack and (asteroid < 0 and stack[-1] > 0):
+                if abs(asteroid) > abs(stack[-1]):
+                    stack.pop()
+                elif abs(asteroid) < abs(stack[-1]):
+                    asteroid = 0
+                elif abs(asteroid) == abs(stack[-1]):
+                    stack.pop()
+                    asteroid = 0
+                    break
+            if asteroid:
+                stack.append(asteroid)
+            
+        return stack
